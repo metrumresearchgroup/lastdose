8e4224a5271cfc7e0a36dcd08b4fe02ef86c29f1
Kyle Baron <kyleb@metrumrg.com>
2019-08-28 22:29:56 -0500

    add covr

---
+++ inst/covr/covr.R
@@ -0,0 +1,31 @@
+library(covr)
+
+options(covr.gcov = "")
+options(covr.flags=c(CFLAGS=""))
+
+src <- "."
+
+tgt <- file.path(tempdir(),"covr")
+
+if(dir.exists(tgt)) unlink(tgt, recursive = TRUE)
+
+dir.create(tgt)
+
+file.copy(src, tgt, recursive = TRUE)
+
+x <- package_coverage(file.path(tgt), quiet=FALSE)
+
+y <- coverage_to_list(x)
+
+z <- zero_coverage(x)
+write.csv(z, file = "inst/maintenance/unit/zero.md")
+
+
+df <- data.frame(file = names(y$filecoverage), coverage = y$filecoverage, row.names=NULL)
+df <- df[order(as.numeric(df$coverage)),]
+
+outfile <- "inst/maintenance/unit/coverage.md"
+cat(file=outfile, "# coverage: ",y$totalcoverage, "%\n\n", sep="")
+cat(file=outfile, append = TRUE, knitr::kable(df,row.names=FALSE),sep="\n")
+
+x
