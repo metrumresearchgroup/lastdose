932b970153aad4cdea1e163775a794433d77116b
Kyle Baron <kyleb@metrumrg.com>
2020-11-12 16:19:20 -0600

    flex ID and TIME columns

--- R/lastdose.R
+++ R/lastdose.R
@@ -4,11 +4,13 @@ NULL
 
 #' Calculate last dose amount (LDOS) and time after last dose (TAD)
 #'
-#' Use [lastdose] to add (or potentially replace) columns to the input
-#' data frame; [lastdose_list] and [lastdose_df] returns calculated information
+#' Use [lastdose()] to add (or potentially replace) columns to the input
+#' data frame; [lastdose_list()] and [lastdose_df()] returns calculated information
 #' as either `list` or `data.frame` format without modifying the input data.
 #'
 #' @param data data set as data frame; see `details`
+#' @param time_col character name for the `TIME` column
+#' @param id_col character name for the subject `ID` column
 #' @param back_calc if `TRUE`, then the time before the first dose
 #' is calculated for records prior to the first dosing record when
 #' at least one dosing record is found in the data set.  Records before
@@ -24,18 +26,18 @@ NULL
 #' @param comments a logcial vector with length equal to the number of rows
 #' in `data` indicating which records are to be ignored when looking for `TAD`
 #' and `LDOS`.  See all the `fill_comments_na` argument.
-#' @param ... arguments passed to [lastdose_list]
+#' @param ... arguments passed to [lastdose_list()]
 #' @param include_ldos `logical`; if `FALSE` then the `LDOS` data is not
-#' appended to the data set.  Only used for the [lastdose] function.
+#' appended to the data set.  Only used for the [lastdose()] function.
 #'
 #' @details
 #'
-#' When calling [lastdose] to modify the data frame, two columns will be
+#' When calling [lastdose()] to modify the data frame, two columns will be
 #' added (by default): `TAD` indicating the time after the most-recent dose
 #' and `LDOS` indicating the amount of the most recent dose.  This default
 #' behavior can be modified with the `include_ldos` argument.
 #'
-#' When calling [lastdose_list] or [lastdose_df], the respective items are
+#' When calling [lastdose_list()] or [lastdose_df()], the respective items are
 #' accessible with `tad` and `ldos` (note the lower case form here to
 #' distinguish from the columns that might be added to the data frame).
 #'
@@ -51,8 +53,8 @@ NULL
 #' - The data set should be formatted according to `NMTRAN` type
 #'   conventions
 #' - Required columns
-#'   - `ID` or `id`: subject identifier
-#'   - `TIME` or `time`: data set time
+#'   - A subject ID column (either `ID` or user-specified)
+#'   - A record time column (either `TIME` or user-specified)
 #'   - `AMT` or `amt`: dose amount for dosing records
 #'   - `EVID` or `evid`: event ID; records with `EVID` or 1 or 4
 #'     are considered dosing records
@@ -97,7 +99,11 @@ lastdose <- function(data,..., include_ldos = TRUE) {
 
 #' @rdname lastdose
 #' @export
-lastdose_list <- function(data, fill = -99, back_calc = TRUE,
+lastdose_list <- function(data,
+                          time_col = "TIME",
+                          id_col = "ID",
+                          fill = -99,
+                          back_calc = TRUE,
                           addl_ties = c("obs_first", "dose_first"),
                           comments = find_comments(data)) {
   if(length(comments)==1) {
@@ -109,24 +115,25 @@ lastdose_list <- function(data, fill = -99, back_calc = TRUE,
   addl_ties <- match.arg(addl_ties)
   sort1 <- addl_ties == "obs_first"
   x <- as.data.frame(data)
-  na <- tolower(names(data))
-  wid <- match("id", na)
+  na <- names(data)
+  lcna <- tolower(na)
+  wid <- match(id_col, na)
   if(is.na(wid)) {
-    stop("column ID is required in the data set.", call.=FALSE)
+    stop("did not find id column `", id_col, "` in `data`", call.=FALSE)
   }
   col_id <- data[[wid]]
   if(!is.numeric(col_id)) {
-    stop("column ID/id is required to be numeric", call.=FALSE)
+    stop("id column is required to be numeric", call.=FALSE)
   }
-  wtime <- match("time", na)
+  wtime <- match(time_col, na)
   if(is.na(wtime)) {
-    stop("column TIME or time is required in the data set.", call.=FALSE)
+    stop("did not find time column `", time_col, "` in `data`", call.=FALSE)
   }
   col_time <- data[[wtime]]
   if(!is.numeric(col_time)) {
-    stop("column TIME/time is required to be numeric", call.=FALSE)
+    stop("time column is required to be numeric", call.=FALSE)
   }
-  wamt <- match("amt", na)
+  wamt <- match("amt", lcna)
   if(is.na(wamt)) {
     stop("column AMT or amt is required in the data set.", call.=FALSE)
   }
@@ -134,7 +141,7 @@ lastdose_list <- function(data, fill = -99, back_calc = TRUE,
   if(!is.numeric(col_amt)) {
     stop("column AMT/amt is required to be numeric", call.=FALSE)
   }
-  wevid <- match("evid",na)
+  wevid <- match("evid",lcna)
   if(is.na(wevid)) {
     stop("column EVID or evid is required in the data set.", call.=FALSE)
   }
@@ -142,7 +149,7 @@ lastdose_list <- function(data, fill = -99, back_calc = TRUE,
   if(!is.numeric(col_evid)) {
     stop("column EVID/evid is required to be numeric", call.=FALSE)
   }
-  waddl <- match("addl", na)
+  waddl <- match("addl", lcna)
   if(is.na(waddl)) {
     col_addl <- vector(mode = "numeric", length=nrow(data))
     wii <- NULL
